b79d4e42bd20d005e85b5b375a622917199d17b7
Nurmuhammad <ashuraliyevnurmuhammad16@gmail.com>
2025-07-27 19:16:03 +0500

    update app.controller.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { CoreModule } from './core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [CoreModule, AuthModule],
+  controllers: [AppController],
   providers: [
     {
       provide: APP_INTERCEPTOR,
